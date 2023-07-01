c04022735cafb7944cfb54af1502c93622f50360
Ashwin <ashwinkv.akv@gmail.com>
2023-07-01 11:39:46 +0530

    F patterns

---
+++ leftdiamond.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+    int i,j,count=4;
+    for ( i = 1; i <= (count*2)-1; i++)
+    {
+        
+        for ( j = 1; j <= count; j++)
+        {
+            if (j<=i && i<=count|| j+i <=count*2&&i>count)
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+        
+    }
+    
+}
